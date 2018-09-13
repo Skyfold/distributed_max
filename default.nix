e27e86d121afdb3e5623ddd5014ecf0ec5799401
pfalzgraf <pfalzgraf@drmartin.com>
2018-09-13 15:43:15 +1000

    updated the overlay to lock it down to a single commit

--- default.nix
+++ default.nix
@@ -1,5 +1,15 @@
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  moz_overlay = import ((import <nixpkgs> {}).fetchFromGitHub 
+    { owner = "mozilla";
+      repo = "nixpkgs-mozilla";
+      inherit 
+       ({ url = "https://github.com/mozilla/nixpkgs-mozilla";
+          rev = "136eacc0ceefa8fb44677799e5639e083606ee5d";
+          sha256 = "04bz093x3zjkzp7ba8mh876a1a34kp3jrys87m79gbln5qvcd2ir";
+          fetchSubmodules = false;
+	}) rev sha256;
+    });
+
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
 in
   ((import ./distributed_max.nix).distributed_max {}).override 
